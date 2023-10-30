feb804ec4eee69cda74f12d8fc8591291537ff96
franfif <francis.barrow@gmail.com>
2023-10-30 17:15:33 -0400

    add django-unicorn

--- Special_Orders/urls.py
+++ Special_Orders/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("orders.urls")),
     path("vendors/", include("vendors.urls")),
+    path("unicorn/", include("django_unicorn.urls")),
 ]
