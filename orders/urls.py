1727b2145fe2485c6a9b1e32ae003b37c22fc45f
franfif <francis.barrow@gmail.com>
2023-11-06 14:13:39 -0500

    fix urls

--- orders/urls.py
+++ orders/urls.py
@@ -5,8 +5,7 @@ from . import views
 app_name = "orders"
 urlpatterns = [
     path("", views.home, name="home"),
-    path("create_order/", views.create_order, name="create_order"),
-    path("<int:order_id>/edit_order", views.edit_order, name="edit_order"),
     path("new_order/", views.OrderCreateView.as_view(), name="create-order"),
+    path("<int:order_id>/edit_order", views.edit_order, name="edit-order"),
     path("trash/", views.trash, name="trash"),
 ]
--- vendors/urls.py
+++ vendors/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = "vendors"
 urlpatterns = [
-    path("", views.list_vendors, name="list_vendors"),
-    path("create_vendor/", views.create_vendor, name="create_vendor"),
-    # path("<int:vendor_id>/edit_vendor", views.edit_vendor, name="edit_vendor"),
+    path("", views.list_vendors, name="list-vendors"),
+    path("create_vendor/", views.create_vendor, name="create-vendor"),
+    # path("<int:vendor_id>/edit_vendor", views.edit_vendor, name="edit-vendor"),
     # path("trash/", views.trash, name="trash"),
 ]
