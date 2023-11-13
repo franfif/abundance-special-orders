5b7e00376212f0b0f5ac422f8d7e73eb707cbfaa
franfif <francis.barrow@gmail.com>
2023-11-13 10:40:58 -0500

    add test to Customer model

--- customers/tests.py
+++ customers/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from .models import Customer, CustomerStatus
+
+
+@pytest.mark.django_db
+def test_customer_model():
+    customer = Customer.objects.create(
+        first_name="Charles",
+        last_name="Dickens",
+        company="Abundance",
+        phone_number="5854542667",
+        status=CustomerStatus.objects.get(status=CustomerStatus.SHAREHOLDER),
+    )
+
+    assert str(customer) == "Dickens, Charles"
+    assert customer.add_margin(50) == (50 / 0.80)
