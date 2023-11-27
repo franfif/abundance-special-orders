3b3a871454a6549347029a16aac126830c38f61e
franfif <francis.barrow@gmail.com>
2023-11-27 09:36:44 -0500

    allow multiline template tags

--- Special_Orders/settings.py
+++ Special_Orders/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import re
+from django.template import base
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,3 +134,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Allow multiline template tags
+base.tag_re = re.compile(base.tag_re.pattern, re.DOTALL)
