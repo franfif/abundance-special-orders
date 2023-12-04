5fb28945b5e7d3d4ece313f5dedf83557478720f
franfif <francis.barrow@gmail.com>
2023-12-04 17:07:58 -0500

    add javascript

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,17 +1,67 @@
 import {AsYouType} from 'libphonenumber-js'
 
-// Execute script when the window has been loaded
-window.onload = function () {
 
-    // Format phone number as the user types it
-    const phoneInput = document.getElementById('id_phone_number');
-    if (phoneInput) {
-        // Add an event listener to the input
-        phoneInput.addEventListener('input', (event) => {
-            const input = event.target;
-            const value = input.value;
-            input.value = new AsYouType('US').input(value);
-        });
-    }
-
+// Format phone number as the user types it
+const phoneInput = document.getElementById('id_phone_number');
+if (phoneInput) {
+    // Add an event listener to the input
+    phoneInput.addEventListener('input', (event) => {
+        const input = event.target;
+        const value = input.value;
+        input.value = new AsYouType('US').input(value);
+    });
 }
+
+// Display New Customer form
+const btn_new_customer = document.getElementById('btn-new-customer');
+const btn_existing_customer = document.getElementById('btn-existing-customer');
+const new_customer_form = document.getElementById('new-customer-form');
+const existing_customer_form = document.getElementById('existing-customer-form');
+
+// Customer does not exist - Replace the customer search with the customer form
+if (btn_new_customer) {
+    btn_new_customer.addEventListener('click', (event) => {
+        new_customer_form.classList.remove('visually-hidden');
+        existing_customer_form.classList.add('visually-hidden');
+        btn_existing_customer.classList.add('btn-secondary');
+        btn_existing_customer.classList.remove('btn-primary');
+        btn_new_customer.classList.add('btn-primary');
+        btn_new_customer.classList.remove('btn-secondary');
+    });
+}
+// Customer exists - Replace the customer form with the customer search
+if (btn_existing_customer) {
+    btn_existing_customer.addEventListener('click', (event) => {
+        existing_customer_form.classList.remove('visually-hidden');
+        new_customer_form.classList.add('visually-hidden');
+        btn_new_customer.classList.add('btn-secondary');
+        btn_new_customer.classList.remove('btn-primary');
+        btn_existing_customer.classList.add('btn-primary');
+        btn_existing_customer.classList.remove('btn-secondary');
+    });
+}
+
+
+// More Info button content
+const btns_order_more_info = document.getElementsByClassName('btn-order-more-info')
+
+for (const btn of btns_order_more_info) {
+    btn.addEventListener('click', (event) => {
+        console.log(btn.getAttribute('aria-expanded'))
+        if (btn.getAttribute('aria-expanded') === 'true') {
+            btn.textContent = "Hide";
+        } else {
+            btn.textContent = "More Info";
+        }
+    });
+}
+
+// Display Bottle Deposit quantity input
+const bottle_deposit_switch = document.getElementById('has_bottle_deposit');
+const bottle_deposit_quantity = document.getElementById('bottle-deposit-quantity');
+
+bottle_deposit_switch.addEventListener('change', (event) => {
+    bottle_deposit_quantity.classList.toggle('visually-hidden');
+});
+
+
