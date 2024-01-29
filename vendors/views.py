a4e025cf4a1d3fb585e2221f3ce337ce4dfb0d5b
franfif <francis.barrow@gmail.com>
2024-01-29 12:40:32 -0500

    add VendorDeleteView

--- vendors/views.py
+++ vendors/views.py
@@ -34,3 +34,17 @@ class VendorUpdateView(generic.UpdateView):
         context["vendor_list"] = Vendor.objects.all()
         context["action"] = "update"
         return context
+
+
+class VendorDeleteView(generic.DeleteView):
+    model = Vendor
+
+    def get_success_url(self):
+        return reverse("vendors:list-vendors")
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the orders
+        context["vendor_list"] = Vendor.objects.all()
+        return context
