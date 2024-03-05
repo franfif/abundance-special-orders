4aef41f3e50cbf84e172fb7ddc29b39c22bc78bd
franfif <francis.barrow@gmail.com>
2024-03-05 12:54:14 -0500

    Add customers app to project's url patterns

--- Special_Orders/urls.py
+++ Special_Orders/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("orders.urls")),
     path("vendors/", include("vendors.urls")),
+    path("customers/", include("customers.urls")),
     path("select2/", include("django_select2.urls")),
 ]
