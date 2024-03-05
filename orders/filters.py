76f6467318195afad837f84dc1a2cad2e7686b3f
franfif <francis.barrow@gmail.com>
2024-03-05 16:56:23 -0500

    Move FilterWithAny base class to project's utils module

--- orders/filters.py
+++ orders/filters.py
@@ -1,23 +1,12 @@
 from django.db.models import Q
 from django_filters import (
-    FilterSet,
-    ChoiceFilter,
     BooleanFilter,
     CharFilter,
     OrderingFilter,
 )
+from utils.filters import FilterWithAny
 from .models import Order
 from .widgets import BooleanRadioSelect
-
-
-class FilterWithAny(FilterSet):
-    def __init__(self, *args, **kwargs):
-        super(FilterWithAny, self).__init__(*args, **kwargs)
-
-        for name, field in self.filters.items():
-            if isinstance(field, ChoiceFilter):
-                # Add "Any" entry to choice fields.
-                field.extra["empty_label"] = "Any " + name
 
 
 class OrderFilter(FilterWithAny):
