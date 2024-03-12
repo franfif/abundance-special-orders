e239ec1df3304968366de0d33d7809cb274ab8e7
franfif <francis.barrow@gmail.com>
2024-03-12 17:08:25 -0400

    Add javascript button add customer to change Plus to Minus

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -153,3 +153,16 @@ function updateOrderStatus(orderId, action) {
         });
 }
 
+const btn_add_customer = document.getElementById('btn-add-customer');
+console.log(btn_add_customer)
+btn_add_customer.addEventListener('click', (event) => {
+    const element = document.getElementById("fa-btn-add-customer");  // Get the DIV element
+
+    if (btn_add_customer.getAttribute('aria-expanded') === 'true') {
+        element.classList.remove("fa-circle-plus");
+        element.classList.add("fa-circle-minus");
+    } else {
+        element.classList.remove("fa-circle-minus");
+        element.classList.add("fa-circle-plus");
+    }
+});
