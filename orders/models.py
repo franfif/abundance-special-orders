98e2e84cb468e8d4f76f0c224ea76264df02820a
franfif <francis.barrow@gmail.com>
2024-03-15 16:40:19 -0400

    Refactor Order.update_status method for clarity

--- orders/models.py
+++ orders/models.py
@@ -112,17 +112,6 @@ class Order(models.Model):
         self.update_status()
 
     def update_status(self):
-        # Not using pending status anymore
-        if self.status == self.PENDING:
-            self.status = self.READY_TO_ORDER
-
-        # Revive a deleted item
-        if self.status == self.DELETED and self.date_deleted is None:
-            self.status = self.INCOMPLETE
-        # Make sure all orders have a status
-        if self.status is None:
-            self.status = self.INCOMPLETE
-
         # Order is deleted
         if self.date_deleted is not None:
             self.status = self.DELETED
@@ -130,6 +119,21 @@ class Order(models.Model):
             return
 
         # TODO: Add Stand-By status update here
+        if self.is_stand_by:
+            # Stand by should not be a status, just a flag on the order displayed
+            pass
+
+        # Not using pending status anymore
+        # All pending orders are switched to ready to order
+        if self.status == self.PENDING:
+            self.status = self.READY_TO_ORDER
+
+        # Revive a deleted item with temporary status
+        if self.status == self.DELETED and self.date_deleted is None:
+            self.status = self.INCOMPLETE
+        # Make sure all orders have a status
+        if self.status is None:
+            self.status = self.INCOMPLETE
 
         # Order is incomplete
         if not self.is_complete():
