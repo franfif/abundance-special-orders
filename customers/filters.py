24ebed9227780f6ab189d3c9408d049e5f235e3c
franfif <francis.barrow@gmail.com>
2024-03-25 11:49:19 -0400

    Clean the input value for phone number search

--- customers/filters.py
+++ customers/filters.py
@@ -12,12 +12,18 @@ class CustomerFilter(FilterWithAny):
     )
 
     def search_customer(self, queryset, name, value):
+        # Clean the input value for phone number search
+        phone_number = "".join(
+            filter(str.isdigit, value)
+        )
+        if phone_number == "":
+            phone_number = "no_phone_search"
         for term in value.split():
             queryset = queryset.filter(
                 Q(first_name__icontains=term)
                 | Q(last_name__icontains=term)
                 | Q(company__icontains=term)
-                | Q(phone_number__icontains=term)
+                | Q(phone_number__contains=phone_number)
             )
         return queryset
 
