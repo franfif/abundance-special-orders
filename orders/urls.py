b5eaa8ed5282881c6c2bb8a683ae51031706ddd4
franfif <francis.barrow@gmail.com>
2024-04-16 11:12:27 -0400

    Clean code

--- orders/urls.py
+++ orders/urls.py
@@ -5,11 +5,10 @@ from . import views
 app_name = "orders"
 urlpatterns = [
     path("", views.OrderListView.as_view(), name="home"),
-    path("orders/<status>/", views.OrderListView.as_view(), name="filtered-orders"),
     path("new_order/", views.OrderCreateView.as_view(), name="create-order"),
     path("<int:pk>/edit_order/", views.OrderUpdateView.as_view(), name="edit-order"),
     path("<int:pk>/delete_order/", views.send_order_to_trash, name="delete-order"),
-    path("<pk>/restore_order/", views.restore_order, name="restore-order"),
+    path("<int:pk>/restore_order/", views.restore_order, name="restore-order"),
     path(
         "order_update_status/<int:order_id>/<action>/",
         views.order_update_status,
