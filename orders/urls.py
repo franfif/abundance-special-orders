7943cae50f90703c202836b0ae23c8648b6548d6
franfif <francis.barrow@gmail.com>
2024-04-16 16:08:25 -0400

    Add back deleted orders url

--- orders/urls.py
+++ orders/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.OrderListView.as_view(), name="home"),
     path("new_order/", views.OrderCreateView.as_view(), name="create-order"),
     path("<int:pk>/edit_order/", views.OrderUpdateView.as_view(), name="edit-order"),
+    path("orders/<status>/", views.OrderListView.as_view(), name="filtered-orders"),
     path("<int:pk>/delete_order/", views.send_order_to_trash, name="delete-order"),
     path("<int:pk>/restore_order/", views.restore_order, name="restore-order"),
     path(
