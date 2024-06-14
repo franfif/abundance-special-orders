6db9b08d0b496741c8c9f0c3f69adf96c87d184e
franfif <francis.barrow@gmail.com>
2024-06-14 10:26:37 -0400

    Refactor filter.js to reduce amount of code with DRY

--- assets/scripts/filters.js
+++ assets/scripts/filters.js
@@ -1,80 +1,33 @@
-const order_filter_form_fields = $('.order-filter .form-select, .order-filter input')
-const customer_filter_form_fields = $('.customer-filter .form-select, .customer-filter input')
+const filter_form_fields = $('.filter .form-select, .filter input')
+const eventType = {'text': 'keyup', 'select-one': 'change', 'radio': 'change'};
 
-function update_list(formData, update_function) {
-    // Ajax request
+function updateList() {
+    // Serialize the form data
+    const formData = $('.filter').serialize();
+
+    // Ajax request to get updated list and change page content
     $.ajax({
         type: 'GET',
         url: 'filter/',
         data: formData,
-        success: update_function,
+        success: (response) => {
+            $('.item-list').html(response.item_list_html);
+        },
         error: function (error) {
             console.error('Error:', error);
         }
     });
 }
 
-function eventUpdateOrderList() {
-    // Serialize the form data
-    const formData = $('.order-filter').serialize();
-
-    // Callback function to update the order list
-    function update_order_list(response) {
-        $('.order-list').html(response.orders_html);
-    }
-
-    update_list(formData, update_order_list)
-}
-
-function eventUpdateCustomerList() {
-    // Serialize the form data
-    const formData = $('.customer-filter').serialize();
-
-    // Callback function to update the customer list
-    function update_customer_list(response) {
-        $('.customer-list').html(response.customers_html);
-    }
-
-    update_list(formData, update_customer_list)
-}
-
-for (const field of order_filter_form_fields) {
+function formFieldEvent(index, field) {
     // Prevent form submission when the Enter key is pressed
     field.addEventListener('keydown', (event) => {
         if (event.key === 'Enter') {
             event.preventDefault();
         }
     });
-    if (field.type === 'text') {
-        // Update order list when text is typed into a text field
-        field.addEventListener('keyup', (event) => {
-            eventUpdateOrderList()
-        });
-    } else {
-        // Update order list when the value of another form field changes
-        field.addEventListener('change', (event) => {
-            eventUpdateOrderList()
-        });
-    }
-
+    // Add updateList function to form fields
+    field.addEventListener(eventType[field.type], updateList);
 }
 
-for (const field of customer_filter_form_fields) {
-    // Prevent form submission when the Enter key is pressed
-    field.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-            event.preventDefault();
-        }
-    });
-    if (field.type === 'text') {
-        // Update customer list when text is typed into a text field
-        field.addEventListener('keyup', (event) => {
-            eventUpdateCustomerList()
-        });
-    } else {
-        // Update customer list when the value of another form field changes
-        field.addEventListener('change', (event) => {
-            eventUpdateCustomerList()
-        });
-    }
-}
+filter_form_fields.each(formFieldEvent)
